12adfa136ac26f7ddedb2ff2ec6a1f9a8682d651
lfflorian <lfflorian4@gmail.com>
2020-05-22 18:07:03 -0600

    archivo de backup

---
+++ robocopy.bat
@@ -0,0 +1,4 @@
+robocopy C:\Users\pablo\Desktop\pff\orgen\A C:\Users\pablo\Desktop\pff\dest\A /mir /log:C:\Users\pablo\Desktop\pff\bk.txt
+robocopy C:\Users\pablo\Desktop\pff\orgen\B C:\Users\pablo\Desktop\pff\dest\B /mir /log+:C:\Users\pablo\Desktop\pff\bk.txt
+robocopy C:\Users\pablo\Desktop\pff\orgen\C C:\Users\pablo\Desktop\pff\dest\C /mir /log+:C:\Users\pablo\Desktop\pff\bk.txt
+pause
